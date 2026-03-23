f2e93726715c3d8ccbbc93b066cf7ab2b93e422e
Triagem App <triagem@app.local>
2026-03-23 19:05:45 -0300

    Removendo linha do secretario do relatorio

--- components/PrintReport.tsx
+++ components/PrintReport.tsx
@@ -156,7 +156,6 @@ export const PrintReport: React.FC<PrintReportProps> = ({ attendees, eventMeta, 
                                 <tr className="row-alt"><td>Instrutor</td><td className="qty-cell">{attendees.filter(a => a.level === Level.INSTRUCTOR).length}</td></tr>
                                 <tr><td>Instrutora</td><td className="qty-cell">{attendees.filter(a => a.ministry === Ministry.INSTRUTORA).length}</td></tr>
                                 <tr><td>Auxiliares</td><td className="qty-cell">{auxiliaresCount}</td></tr>
-                                <tr className="row-alt"><td>Secretário (a) da música</td><td className="qty-cell">2</td></tr>
                             </tbody>
                         </table>
 
